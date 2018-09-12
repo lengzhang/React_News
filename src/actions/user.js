0d1cfe2c8fac2516ed395fb44b33086b9af374d3
Leng <lengzhang@live.com>
2018-09-12 11:37:51 -0700

    Revert "React news"

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,144 +1,59 @@
 import Ajax from '../common/ajax'
-import Fetch from '../common/fetch'
-
-import nodeFetch from 'node-fetch'
-
-import { getUserInfo } from '../reducers/user'
 
 // 储存accessToken到redux
-export function saveAccessToken({
-    accessToken
-}) {
-    return dispatch => {
-        dispatch({
-            type: 'SAVE_ACCESS_TOKEN',
-            accessToken
-        })
-    }
+export function saveAccessToken({ accessToken }) {
+  return dispatch => {
+    dispatch({ type: 'SAVE_ACCESS_TOKEN', accessToken })
+  }
 }
 
-export function saveUserInfo({
-    userinfo
-}) {
-    return dispatch => {
-        dispatch({
-            type: 'SAVE_USERINFO',
-            userinfo
-        })
-    }
+export function saveUserInfo({ userinfo }) {
+  return dispatch => {
+    dispatch({ type: 'SAVE_USERINFO', userinfo })
+  }
 }
 
-export function signUp({account, password, confirmPassword}) {
-    return (dispatch, getState) => {
-        return new Promise(async (resolve, reject) => {
+export function signIn({ nickname }) {
+  return dispatch => {
+  return new Promise(async (resolve, reject) => {
 
-            let signupURL = `http://newsapi.gugujiankong.com/Handler.ashx?action=register&r_userName=${account}&r_password=${password}&r_confirmPassword=${confirmPassword}`
-            let signupOptions = {
-                method: 'GET'
-            }
-            let response = await Fetch({url: signupURL, options: signupOptions});
+    // 这里写你的登录请求，登录成功以后，将token储存到cookie，使用httpOnly(比较安全)
+    // your code ...
 
-            resolve(response);
-        })
+    // 储存 cookie
+    let [ err, data ] = await Ajax({
+      url: window.location.origin+'/sign/in',
+      method: 'post',
+      data: {
+        nickname: nickname
+      }
+    })
+
+    if (data && data.success) {
+      resolve([null, true])
+    } else {
+      resolve(['sign error'])
     }
-}
 
-export function signIn({account, password}) {
-    return (dispatch, getState) => {
-        return new Promise(async (resolve, reject) => {
-
-            // 这里写你的登录请求，登录成功以后，将token储存到cookie，使用httpOnly(比较安全)
-            // your code ...
-
-            // 配置 fetch url, options
-            let signinURL = `http://newsapi.gugujiankong.com/Handler.ashx?action=login&username=${account}&password=${password}&r_userName=${account}&r_password=${password}&r_confirmPassword=${password}`
-            let signinOptions = {
-                method: 'GET'
-            }
-
-            let response = await Fetch({url: signinURL, options: signinOptions});
-
-            if (response != null) {
-                // 储存 cookie
-                let [err, data] = await Ajax({
-                    url: window.location.origin + '/sign/in',
-                    method: 'post',
-                    data: {
-                        userInfo: {
-                            id: response.UserId,
-                            account: response.NickUserName
-                        }
-                    }
-                })
-
-                if (data && data.success) {
-                    resolve([null, true])
-                } else {
-                    resolve(['sign error'])
-                }
-            }
-            else {
-                resolve(['sign error'])
-            }
-        })
-    }
+  })
+  }
 }
 
 export function signOut() {
-    return dispatch => {
-        return new Promise(async (resolve, reject) => {
+  return dispatch => {
+  return new Promise(async (resolve, reject) => {
 
-            let [err, data] = await Ajax({
-                url: window.location.origin + '/sign/out',
-                method: 'post'
-            })
+    let [ err, data ] = await Ajax({
+      url: window.location.origin+'/sign/out',
+      method: 'post'
+    })
 
-            if (data && data.success) {
-                resolve([null, true])
-            } else {
-                resolve(['sign error'])
-            }
-
-        })
+    if (data && data.success) {
+      resolve([null, true])
+    } else {
+      resolve(['sign error'])
     }
-}
 
-export function loadUserComments() {
-    return (dispatch, getState) => {
-        return new Promise(async (resolve, reject) => {
-            let userinfo = await getUserInfo(getState());
-            let url = `http://newsapi.gugujiankong.com/Handler.ashx?action=getusercomments&userid=${userinfo.id}`
-            let options = {
-                method: 'GET'
-            }
-
-            let response = await nodeFetch(url, options).then(response => response.json());
-            await dispatch({
-                type: 'SAVE_USERCOMMENTS',
-                comments: response
-            });
-            resolve(null);
-
-        })
-    }
-}
-
-export function loadUserCollections() {
-    return (dispatch, getState) => {
-        return new Promise(async (resolve, reject) => {
-            let userinfo = await getUserInfo(getState());
-            let url = `http://newsapi.gugujiankong.com/Handler.ashx?action=getuc&userid=${userinfo.id}`
-            let options = {
-                method: 'GET'
-            }
-
-            let response = await nodeFetch(url, options).then(response => response.json());
-            await dispatch({
-                type: 'SAVE_USERCOLLECTIONS',
-                collections: response
-            });
-            resolve(null);
-
-        })
-    }
+  })
+  }
 }
